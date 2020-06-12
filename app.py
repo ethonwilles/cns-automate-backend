7df6c4aabf7bb36084be6c74f3112db24c571d5f
ethonwilles <ethonwilles@gmail.com>
2020-06-12 11:17:35 -0600

    asdf

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def todo_check():
 
         client = Client(os.getenv("account_sid"), os.getenv("twilio_auth"))
         message = client.messages.create(
-            to="+18016912737",
+            to="+18017068523",
             from_=f"{os.getenv('number')}",
             body=f"Task '{task}' was marked as completed today."
         )
